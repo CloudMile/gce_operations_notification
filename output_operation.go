5b38aa9bab35c817466a0b822971eb4131722c35
C.lin Yeh <demeter.yeh@gmail.com>
2018-07-27 10:32:18 +0800

    [TITLE] use `go` to multi get datastore

--- output_operation.go
+++ output_operation.go
@@ -53,27 +53,43 @@ func (outO *OutputOperations) SendMail() {
 }
 
 func gceUnkownAggregatedList(ctx context.Context, computeService *compute.Service, project string) (unkownAggregatedList map[string][]*OutputOperation) {
-	unkownAggregatedListTemp := make(map[string][]*OutputOperation)
+	unkownAggregatedList = make(map[string][]*OutputOperation)
 	req := computeService.GlobalOperations.AggregatedList(project)
 	req = req.Filter(os.Getenv("OPERATION_FILTER"))
 
 	if err := req.Pages(ctx, func(page *compute.OperationAggregatedList) error {
 		for _, operationsScopedList := range page.Items {
-			if len(operationsScopedList.Operations) > 0 {
-				for _, operation := range operationsScopedList.Operations {
-					log.Infof(ctx, "id: %d, target %s", operation.Id, operation.TargetLink)
-					isNew, outputOperation := getOrPutDatastore(ctx, operation)
-					if isNew {
-						unkownAggregatedListTemp[operation.OperationType] = append(unkownAggregatedListTemp[operation.OperationType], &outputOperation)
-					}
-				}
+			lenOperationsScopedListOperations := len(operationsScopedList.Operations)
+
+			if lenOperationsScopedListOperations > 0 {
+				unkownAggregatedList = multiCheckOperation(ctx, lenOperationsScopedListOperations, operationsScopedList)
 			}
 		}
 		return nil
 	}); err != nil {
 		log.Errorf(ctx, "compute error: %s", err)
 	}
-	return unkownAggregatedListTemp
+	return
+}
+
+func multiCheckOperation(ctx context.Context, arrayLen int, operationsScopedList compute.OperationsScopedList) (unkownAggregatedList map[string][]*OutputOperation) {
+	unkownAggregatedList = make(map[string][]*OutputOperation)
+	var c = make(chan bool, arrayLen)
+
+	for _, operation := range operationsScopedList.Operations {
+		go func(operation *compute.Operation) {
+			log.Infof(ctx, "id: %d, target %s", operation.Id, operation.TargetLink)
+			isNew, outputOperation := getOrPutDatastore(ctx, operation)
+			if isNew {
+				unkownAggregatedList[operation.OperationType] = append(unkownAggregatedList[operation.OperationType], &outputOperation)
+			}
+			c <- true
+		}(operation)
+	}
+	for range operationsScopedList.Operations {
+		<-c
+	}
+	return
 }
 
 func getOrPutDatastore(ctx context.Context, operation *compute.Operation) (newDatastore bool, datastoreObject OutputOperation) {
@@ -81,7 +97,7 @@ func getOrPutDatastore(ctx context.Context, operation *compute.Operation) (newDa
 	datastoreKey := datastore.NewKey(ctx, GAEEntityKind, strconv.FormatUint(operation.Id, 10), 0, nil)
 
 	if err := datastore.Get(ctx, datastoreKey, &datastoreObject); err != nil {
-		log.Errorf(ctx, "datastore %s", err)
+		log.Warningf(ctx, "datastore %s", err)
 		if err.Error() == "datastore: no such entity" {
 			datastoreObject.TargetLink = operation.TargetLink
 			datastoreObject.OperationType = operation.OperationType
